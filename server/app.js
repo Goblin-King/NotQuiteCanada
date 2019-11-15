492351c6036c8ff0597c3b3ecebd3e169cc84cdc
root <root@ip-172-31-1-234.us-east-2.compute.internal>
2019-11-15 15:55:49 +0000

    Added time till election

--- server/app.js
+++ server/app.js
@@ -2,15 +2,38 @@ var express = require('express');
 var app = express(); 
 var http = require('http'); 
 var fs = require('fs');
-var MongoClient = require('mongodb').MongoClient;
-const uri = mongodb+srv://LGBTDetroit:LGBTDetroit@cluster0-f4w2b.mongodb.net/test?retryWrites=true&w=majority
-app.set('views','/home/ec2-user/not_quite_canada/views');
+
+
+
+
+//var MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://LGBTDetroit:LGBTDetroit@cluster0-f4w2b.mongodb.net/test?retryWrites=true&w=majority"
+//const client = new MongoClient(uri, {useNewUrlParser:true });
+
+app.set('views','/home/ec2-user/NotQuiteCanada/server/views');
 app.set('view engine', "ejs");
 app.get('/', function (req,res) {
-	res.render('index',{});
+	res.render('main',{});
+});
+app.get('/main.ejs', function (req,res) {
+	res.render('main',{});
+});
+app.get('/registration.ejs', function (req,res) {
+	res.render('registration',{});
 });
 
-app.listen(5000, function() {
-	console.log('Example app listening on port 5000!');
+app.get('/events.ejs', function (req,res) {
+	res.render('registration',{});
+//	client.connect(err=>{
+//	const collection = client.db("NotQuiteCanada").collection("Community Events");
+//	collection.find({location:"Detroit, MI"}).toArray(function(err,result){
+//	if (err) throw err;
+	
+//});
+//	});
+});
+
+app.listen(3500, function() {
+	console.log('Example app listening on port 3500!');
 });
 
--- server/views/main.js
+++ server/views/main.js
@@ -1,3 +1,10 @@
 function dropNavFunc(){
     document.getElementById("dropDown").classList.toggle("show");
 }
+function daysTillElection{
+var election_date = new Date(2019,10,3);
+ var now_date = new Date( );
+ var Difference_In_Time = election_date.getTime() - now_date.getTime();
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+return “It’s <br>” Difference_In_Days “until the general election. Register to make your vote count!"):
+}
