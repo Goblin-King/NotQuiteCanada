b97f309b88ad534095adf1e670a0c7029a437d7d
root <root@ip-172-31-1-234.us-east-2.compute.internal>
2019-11-15 01:44:29 +0000

    Moved UI to server/views, added mongodb

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express(); 
 var http = require('http'); 
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+const uri = mongodb+srv://LGBTDetroit:LGBTDetroit@cluster0-f4w2b.mongodb.net/test?retryWrites=true&w=majority
 app.set('views','/home/ec2-user/not_quite_canada/views');
 app.set('view engine', "ejs");
 app.get('/', function (req,res) {
